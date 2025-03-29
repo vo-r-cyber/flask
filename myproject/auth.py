9588747225526f2dd75d58f9ac7df6c3e9b15ca0
vo-r-cyber <seoyeonhong@unist.ac.kr>
2025-03-29 17:24:13 +0900

    ..

--- myproject/auth.py
+++ myproject/auth.py
@@ -8,32 +8,34 @@ from myproject.db import get_db
 
 bp=Blueprint('auth',__name__,url_prefix='/auth')
 
-@bp.route('/register',methods=('GET','POST'))
+@bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
         username = request.form['username']
-        password=request.form['password']
-        db=get_db()
-        error=None
+        password = request.form['password']
+        db = get_db()
+        error = None
 
         if not username:
-            error: 'Username is required.'
+            error = 'Username is required.'
         elif not password:
-            error: 'Password is required.'
+            error = 'Password is required.'
 
-            if error is None:
-                try:
-                    db.excute(
-                        "INSERT INTO user (username, password) VALUES (?,?)"
-                        (username, generate_password_hash(password)),
-                    )
-                    db.commit()
-                except db.IntegrityError:
-                    error=f"User {username} is already registered."
-                else:
-                    return redirect (url_for("auth.login"))
-            flash(error)
-        return render_template('auth/register.html')
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO user (username, password) VALUES (?, ?)",
+                    (username, generate_password_hash(password)),
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f"User {username} is already registered."
+            else:
+                return redirect(url_for("auth.login"))
+
+        flash(error)
+    return render_template('auth/register.html')
+
 
 @bp.route('/login',methods=('GET','POST'))
 def login():
@@ -42,9 +44,9 @@ def login():
         password = request.form['password']
         db=get_db()
         error=None
-        user=db.excute(
+        user=db.execute(
             'SELECT*FROM user WHERE username =?', (username,)
-        ),fetchone()
+        ).fetchone()
 
         if user is None:
             error='Incorrect username.'
